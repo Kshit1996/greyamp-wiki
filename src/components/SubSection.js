8bda764819c8c941a8cc11ddde3a25023744e07f
kshitiz <kshit96@gmail.com>
2020-07-20 14:47:14 +0700

    Added Hyperlink to open in new tab

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -7,13 +7,15 @@ import {CaretRightFill} from "react-bootstrap-icons";
 export default function SubSection(props) {
   const [activeClass, setActiveClass] = useState("caret-down");
 
-
   function createListGroup(subtopics) {
     return (
         <ListGroup variant="flush">
           {subtopics.map(
               subtopic => <ListGroup.Item
-                  key={subtopic[0]} onClick={()=>props.onClick(props.chapter,subtopic[0])}>{subtopic[0]}</ListGroup.Item>
+                  key={subtopic[0]}
+                  onClick={() => props.onClick(props.chapter, subtopic[0])}><a
+                  href={subtopic[1]}
+                  target="_blank">{subtopic[0]}</a></ListGroup.Item>
           )}
         </ListGroup>
     )
@@ -33,7 +35,8 @@ export default function SubSection(props) {
           <Accordion.Toggle onClick={changeActiveClass} as={Card.Header}
                             variant="link"
                             eventKey={props.chapter}>
-            <CaretRightFill className={activeClass}/><b> &nbsp; {props.chapter}</b>
+            <CaretRightFill
+                className={activeClass}/><b> &nbsp; {props.chapter}</b>
           </Accordion.Toggle>
           <Accordion.Collapse eventKey={props.chapter}>
             <Card.Body>{createListGroup(props.subtopics)}</Card.Body>
