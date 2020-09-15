281ea747f1c49a6dfa9c5ca2420ad316c2e8bdce
Nidhi Mittal <nidhi.mittal@greyamp.com>
2020-09-15 13:39:20 +0530

    Added css for subsection title and onSelect

--- src/components/SubSection.js
+++ src/components/SubSection.js
@@ -11,7 +11,7 @@ export default function SubSection(props) {
     return (
         <ListGroup variant="flush">
           {subtopics.map(
-              subtopic => <ListGroup.Item
+              subtopic => <ListGroup.Item action={true}
                   key={subtopic}
                   onClick={() => props.onClick(props.chapter, subtopic)}>
                 {subtopic}
