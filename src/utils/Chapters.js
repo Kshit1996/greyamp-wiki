ef9e9d6ce9a0ddbe5be13efa9c4f8da1b57e6c9b
Nidhi Mittal <nidhi.mittal@greyamp.com>
2020-09-15 13:20:02 +0530

    Created utils files to remove hardcoded values

---
+++ src/utils/Chapters.js
@@ -0,0 +1,8 @@
+import {orgAndGovernanceSubTopics} from "./SubTopics";
+import {orgAndGovernanceLinks} from "./Documents";
+
+export const chapters=[{
+    title: "Organisation & Governance",
+    subTopics: orgAndGovernanceSubTopics,
+    documents: orgAndGovernanceLinks
+}]
